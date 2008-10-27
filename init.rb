2d60adf3ebd505053a6d9d797f7f022537940066
Alex MacCaw <maccman@gmail.com>
2008-10-27 21:06:37 +0800

    Register JavaScript expansion properly
    Signed-off-by: Bodaniel Jeanes <me@bjeanes.com>

--- init.rb
+++ init.rb
@@ -7,6 +7,8 @@ end
 
 ActionView::Base.send(:include, Juggernaut::JuggernautHelper)
 
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :juggernaut => ['juggernaut/swfobject', 'juggernaut/juggernaut']
+
 ActionController::Base.class_eval do
   alias_method :render_without_juggernaut, :render
   include Juggernaut::RenderExtension
